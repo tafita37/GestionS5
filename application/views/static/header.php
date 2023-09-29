8f1ea8bfeda9fe4cd9df976d81c77cf1acdd32d8
tafita37 <118960434+tafita37@users.noreply.github.com>
2023-09-29 19:47:09 +0300

    First Commit

---
+++ application/views/static/header.php
@@ -0,0 +1,78 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>HumanResources &mdash; Free Business Website Template by Colorlib</title>
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+</head>
+<body>
+
+  <div class="js-animsition animsition" id="site-wrap" data-animsition-in-class="fade-in" data-animsition-out-class="fade-out">
+
+
+    <header class="templateux-navbar" role="banner">
+
+      <div class="container"  data-aos="fade-down">
+        <div class="row">
+
+          <div class="col-3 templateux-logo">
+            <a href="index.html" class="animsition-link">HumanResources</a>
+          </div>
+          <nav class="col-9 site-nav">
+            <button class="d-block d-md-none hamburger hamburger--spin templateux-toggle templateux-toggle-light ml-auto templateux-toggle-menu" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+              </span>
+            </button> <!-- .templateux-toggle -->
+
+            <ul class="sf-menu templateux-menu d-none d-md-block">
+              <li class="active">
+                <a href="index.html" class="animsition-link">Home</a>
+              </li>
+              <li><a href="about.html" class="animsition-link">About</a></li>
+              <li>
+                <a href="services.html" class="animsition-link">Services</a>
+                <ul>
+                  <li><a href="#">HR Consulting</a></li>
+                  <li><a href="#">Leadership Training</a></li>
+                  <li>
+                    <a href="#">HR Management</a>
+                    <ul>
+                      <li><a href="#">Operational Management</a></li>
+                      <li><a href="#">Corporate Program</a></li>
+                      <li>
+                        <a href="#">Service 3</a>
+                        <ul>
+                          <li><a href="#">Service 1</a></li>
+                          <li><a href="#">Service 2</a></li>
+                          <li><a href="#">Service 3</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="blog.html" class="animsition-link">Blog</a></li>
+              <li><a href="contact.html" class="animsition-link">Contact</a></li>
+            </ul> <!-- .templateux-menu -->
+
+          </nav> <!-- .site-nav -->
+          
+
+        </div> <!-- .row -->
+      </div> <!-- .container -->
+    </header> <!-- .templateux-navba -->
+    
+    
+    <div class="templateux-cover" style="background-image: url(<?php echo base_url("assets/images/hero_1.jpg"); ?>);">
+      <div class="container">
+        <div class="row align-items-lg-center">
+
+          <div class="col-lg-6 order-lg-1 text-center mx-auto">
+            <h1 class="heading mb-3 text-white" data-aos="fade-up">Blog Single Post</h1>
+            <p class="lead mb-5 text-white" data-aos="fade-up"  data-aos-delay="100">July 3, 2018 &bullet; by <a href="#" class="text-white">John Smith</a></p>
+          </div>
+
+        </div>
+      </div>
+    </div> <!-- .templateux-cover -->
